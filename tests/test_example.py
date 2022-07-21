bca3413f9e783af35b9822e5a59b10898143dec1
Evan Rosson <github@erosson.org>
2022-07-21 00:11:05 -0400

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,6 +3,10 @@ import pytest
 def test_passes():
     assert 1 + 1 == 2
 
-@pytest.mark.skip(reason="Never ever leave failing tests in the main branch! It's better to quickly skip the test and fix it later, than to let it keep failing for hours.")
+@pytest.mark.skip(reason="Never ever leave failing tests in the main branch!")
 def test_fails():
+    """Failing tests in the main branch block any other changes!
+    
+    If you can't quickly fix a failing test, it's better to mark it skipped and come back later, than to let it keep failing for hours.
+    """
     assert False
