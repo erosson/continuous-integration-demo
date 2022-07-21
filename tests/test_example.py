49b8eda8f605579461bdd8e1f30213ec98105c8f
Evan Rosson <github@erosson.org>
2022-07-21 00:08:16 -0400

    Create test_example.py

---
+++ tests/test_example.py
@@ -0,0 +1,6 @@
+def test_passes():
+    assert 1 + 1 == 2
+
+@pytest.mark.skip(reason="Never ever leave failing tests in the main branch! It's better to quickly skip the test and fix it later, than to let it keep failing for hours.")
+def test_fails():
+    assert False
